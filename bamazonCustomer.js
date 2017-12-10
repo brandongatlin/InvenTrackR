84eaa35457ce90071d686fe26ac9145974474935
brandongatlin <brandongatlin1981@me.com>
2017-12-10 00:11:00 -0600

    added results to query so I can check user desired with number in stock. If user chooses too many, cl, oops.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,6 +5,8 @@ var inquirer = require("inquirer");
 var item;
 var price;
 var id;
+var storeQuantity;
+var chosenQuantity;
 
 //sql login info
 var connectObject = {
@@ -32,19 +34,19 @@ connection.connect(function(error) {
 //function to display all items in store
 function display() {
 
-  connection.query("SELECT item_id, product_name, price FROM products", function(err, results) {
+  connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function(err, results) {
     if (err) throw err;
 
     for (var i = 0; i < results.length; i++) {
       id = results[i].item_id;
       item = results[i].product_name;
       price = results[i].price;
+      storeQuantity = results[i].stock_quantity;
 
-      console.log("id:" + id + ",", item, "$" + price);
+      // console.log("loop storeQuantity: ", storeQuantity);
+
+      console.log("id:" + id + ",", item, "$" + price, "in stock: " + storeQuantity);
     }
-
-
-
 
     // once you have the items, prompt the user for which they'd like to bid on
     inquirer.prompt([{
@@ -58,7 +60,7 @@ function display() {
         return false;
       }
     }]).then(function(answer) {
-      console.log(answer);
+      // console.log(answer);
       inquirer.prompt([{
         name: "quantity_choice",
         type: "input",
@@ -68,8 +70,25 @@ function display() {
             return true;
           }
           return false;
+        } //end if statement
+      }]).then(function(answer) {
+        // console.log(answer);
+        chosenQuantity = answer.quantity_choice;
+
+        // console.log("chosen quantity: ", chosenQuantity);
+        // console.log("store quantity: ", storeQuantity);
+        if (chosenQuantity > storeQuantity) {
+          console.log("oops, not enough");
         }
-      }]);
+      });
     });
   });
 } // end display function
+
+// if (answer.item_choice > results[i].quantity) {
+//   console.log("We don't have enough!");
+
+//start function checkQuantity to check if store has sufficient quantitiy for user.
+// function checkQuantity() {
+//
+// }
