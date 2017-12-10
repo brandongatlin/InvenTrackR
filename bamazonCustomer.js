69e264375c3643f783d434e31d3bc2da9f42e764
brandongatlin <brandongatlin1981@me.com>
2017-12-09 22:14:46 -0600

    added prompt to ask user to input item id they'd like to purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -34,55 +34,12 @@ function display() {
     console.log(results);
 
     // once you have the items, prompt the user for which they'd like to bid on
-    // inquirer
-    //   .prompt([{
-    //       name: "choice",
-    //       type: "list",
-    //       choices: function() {
-    //         var choiceArray = [];
-    //         for (var i = 0; i < results.length; i++) {
-    //           choiceArray.push(results[i].item_name);
-    //         }
-    //         return choiceArray;
-    //       },
-    //       message: "What auction would you like to place a bid in?"
-    //     },
-    //     {
-    //       name: "bid",
-    //       type: "input",
-    //       message: "How much would you like to bid?"
-    //     }
-    //   ])
-    //   .then(function(answer) {
-    //     // get the information of the chosen item
-    //     var chosenItem;
-    //     for (var i = 0; i < results.length; i++) {
-    //       if (results[i].item_name === answer.choice) {
-    //         chosenItem = results[i];
-    //       }
-    //     }
-    //
-    //     // determine if bid was high enough
-    //     if (chosenItem.highest_bid < parseInt(answer.bid)) {
-    //       // bid was high enough, so update db, let the user know, and start over
-    //       connection.query(
-    //         "UPDATE auctions SET ? WHERE ?", [{
-    //             highest_bid: answer.bid
-    //           },
-    //           {
-    //             id: chosenItem.id
-    //           }
-    //         ],
-    //         function(error) {
-    //           if (error) throw err;
-    //           console.log("Bid placed successfully!");
-    //           start();
-    //         }
-    //       );
-    //     } else {
-    //       // bid wasn't high enough, so apologize and start over
-    //       console.log("Your bid was too low. Try again...");
-    //     }
-    //   });
+    inquirer
+      .prompt([{
+        name: "choice",
+        type: "input",
+        message: "Which item would you like to purchase?"
+
+      }]);
   });
 } // end display function
